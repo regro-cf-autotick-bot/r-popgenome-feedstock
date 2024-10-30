c34bb4692000a8efc7fbae2514608b544e2d7d93
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-30 01:20:16 -0700

    fix macOS version

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+# Delete mmacosx-version-min from Makeconf
+# See https://github.com/conda-forge/r-base-feedstock/issues/350
+sed -i 's/-mmacosx-version-min=10.13//' ${PREFIX}/lib/R/etc/Makeconf
+
 export DISABLE_AUTOBREW=1
 $R CMD INSTALL --build . ${R_ARGS}
